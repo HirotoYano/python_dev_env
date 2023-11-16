6dfc21ef3d27160c6ac5ce6b66a21c6968d6025b
HirotoYano <mitune@mc2wifi067073.it.osakac.ac.jp>
2023-11-16 14:50:30 +0900

    [fix] Added processing sorting by options.

--- env_launch.sh
+++ env_launch.sh
@@ -1,21 +1,57 @@
 #!/usr/bin/env fish
 
-set container_name "$1"
+function remove_container
+    set container_name $argv[1]
 
-set container_id (docker ps -q -f "name=$container_name")
-
-if test -n "$container_id"
-    docker-compose down --rmi all
-    echo "コンテナ$container_nameを削除しました"
+    set container_id (docker ps -q -f "name=$container_name")
+    if test -n "$container_id"
+        docker-compose down --rmi all
+        echo "コンテナ $container_name を削除しました"
+    else
+        echo "コンテナ $container_name は存在しません"
+    end
 end
 
-if docker-compose build --no-cache
-    docker-compose up -d
-    echo "コンテナ$container_nameを正常に起動しました"
+function start_container
+    set container_name $argv[1]
+
+    if docker-compose build --no-cache
+        docker-compose up -d
+        echo "コンテナ $container_name を正常に起動しました"
+        exec_container $container_name
+    else
+        set error_code $status
+        echo "エラーコード: $error_code"
+    end
+end
+
+function exec_container
+    set container_name $argv[1]
     set container_id (docker ps -q -f "name=$container_name")
-    docker exec -it $container_id bash
-else
-    set error_code $status
-    echo $error_code
+
+    if test -n "$container_id"
+        docker exec -it $container_id bash
+    else
+        echo "コンテナ $container_name は存在しません。"
+    end
+end
+
+set container_name ""
+
+for arg in $argv
+    switch $arg
+        case "-d"
+            set container_name $argv[2]
+            remove_container $container_name
+            break
+        case "-s"
+            set container_name $argv[2]
+            start_container $container_name
+            break
+        case "-e"
+            set container_name $argv[2]
+            exec_container $container_name
+            break
+    end
 end
 
